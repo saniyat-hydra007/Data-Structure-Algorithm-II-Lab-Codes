05fa736ce72fcd545506774504ded24e44e13fe8
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-03-14 13:14:20 +0600

    updated

--- Devide And Conqure/BinSearch.cpp
+++ Devide And Conqure/BinSearch.cpp
@@ -1,21 +1,28 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-bin_search(A[],len,key){
-        l = 0;
-        h = len-1;
-        index = -1;
+bin_search(int A[], int n, int key){
+        int l = 0;
+        int h = n-1;
+        int index = -1;
         while(l <= h) {
-                mid = (l+h)/2;
+                int mid = (l+h)/2;
                 if(A[mid] == key) {
                         index = mid;
                         break;
                 }
                 else if(A[mid] >key) {
-                        H = mid-1;
+                        h = mid-1;
                 }
                 else{
                         l=mid+1;
                 }
         }
         return index;
+}
+int main(){
+        int arr[] = {2,5,7,8,9,13,16,19,33,43};
+
+        cout << "Index is: " << bin_search(arr,10,16) << endl;
+        return 0;
 }
