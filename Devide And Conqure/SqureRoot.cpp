60cfbf74540f37c429a977a7930eac9fbaa27bf9
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-03-14 13:14:43 +0600

    Updated

---
+++ Devide And Conqure/SqureRoot.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+absulate(float x){
+        if (x>=0) {
+                return x;
+        }                 else
+                return -x;
+}
+
+sqrt(float x){
+        float l = 0;
+        float h = x;
+        float result = 0;
+        while(l) {
+                float mid = (l+h)/2;
+                if(absulate(mid*mid-x) < 0.000001) {
+                        result = mid;
+                        break;
+                }
+                else if(mid*mid >x) {
+                        h = mid;
+                }
+                else
+                        l = mid;
+        }
+        return result;
+}
+
+int main(){
+        cout << sqrt(64.64) << endl;
+}
