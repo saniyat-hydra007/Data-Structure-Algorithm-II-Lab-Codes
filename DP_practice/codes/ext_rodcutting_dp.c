4f9ffe660b6b74ac1128605a47fe4a10162ddcdd
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-04-26 02:56:53 +0600

    Create ext_rodcutting_dp.c

---
+++ DP_practice/codes/ext_rodcutting_dp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define INF 99999
+
+int r[1000];
+int cut[1000];
+
+int rodcutting(int p[], int n) {
+    // write your code here
+}
+
+void print_solution(int n) {
+    // write your code here
+}
+
+int main() {
+    int p[] = {INF, 1, 5, 8, 9, 10}; // don't use index 0
+    int n = 5;
+    int max_rev = rodcutting(p, n);
+    printf("%d\n", max_rev);
+    print_solution(n);
+    return 0;
+}
