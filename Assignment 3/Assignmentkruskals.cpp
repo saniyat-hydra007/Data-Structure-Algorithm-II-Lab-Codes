c51c931cc21aa9ed2b08d5d36f4ab3843b975084
Saniyat Mushrat Lamim <70531660+saniyat-hydra007@users.noreply.github.com>
2021-06-01 20:38:57 +0600

    Update Assignmentkruskals.cpp

--- Assignment 3/Assignmentkruskals.cpp
+++ Assignment 3/Assignmentkruskals.cpp
@@ -39,3 +39,90 @@ const int N = 1e3+2, M = 3e5+5, OO = 0x3f3f3f3f;
 // int dx[] = {1,-1,1,-1,2,2,-2,-2} , dy[] = {2,2,-2,-2,1,-1,1,-1}; // Knight Direction
 // int dx[] = {2,-2,1,1,-1,-1} , dy[] = {0,0,1,-1,1,-1}; // Hexagonal Direction
 
+#define N_VERTICES 11
+
+
+class DisjointSet
+{
+int p[N_VERTICES];
+
+public:
+void make_set(int x)
+{
+        p[x] = x;
+}
+int find_set(int x)
+{
+        while(x != p[x])
+        {
+                x = p[x];
+        }
+        return x;
+}
+void union_set(int x, int y)
+{
+        int a = find_set(x);
+        int b = find_set(y);
+        p[a] = b;
+}
+bool same_set(int x, int y)
+{
+        if(find_set(x) == find_set(y))
+                return true;
+        return false;
+}
+};
+
+struct Edge {
+        int u;
+        int v;
+        int weight;
+};
+
+int cmpfunc(const void * a, const void * b) {
+        // return ((Edge*)a)->weight > ((Edge*)b)->weight;
+        // the mistake was to use > instead of -
+        return ((Edge*)a)->weight - ((Edge*)b)->weight;
+}
+
+class Graph
+{
+public:
+int mat[N_VERTICES][N_VERTICES];
+void init_graph()
+{
+        for(int i=0; i<N_VERTICES; i++)
+        {
+                for(int j=0; j<N_VERTICES; j++)
+                {
+                        mat[i][j] = 0;
+                }
+        }
+}
+void add_edge(int u, int v, int weight)
+{
+        mat[u][v] = weight;
+        mat[v][u] = weight;
+}
+
+int create_edge_list(Edge edgelist[]) {
+        int c = 0;
+        for(int i=0; i<N_VERTICES; i++) {
+                for(int j=i; j<N_VERTICES; j++) {
+                        if(mat[i][j] != 0) {
+                                Edge e;
+                                e.u = i;
+                                e.v = j;
+                                e.weight = mat[i][j];
+                                edgelist[c] = e;
+                                c++;
+                        }
+                }
+        }
+        return c;
+}
+
+void sort_edge_list(Edge edgelist[], int n) {
+        qsort(edgelist, n, sizeof(Edge), cmpfunc);
+}
+
